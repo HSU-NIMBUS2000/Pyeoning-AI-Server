a89c0908549492272afbaae9b183ee907e8d2a15
khyaejin <olivia019182@naver.com>
2024-11-01 04:36:26 +0900

    ✨ [Feat] 요약보고서 생성 - response 타입 일치하도록 수정

--- api/doctor_ai/routers.py
+++ api/doctor_ai/routers.py
@@ -21,17 +21,31 @@ async def chatbot_function(request: AiChatRequest):
     )
     return {"response": response}
 
+
 # 요약 기능 라우터
 @router.post("/api/doctor-ai/summarize")
 async def summary_function(request: AiSummationRequest):
-    # 모델이 어떤 역할을 수행할 지 설정
-    system_content = "당신은 환자의 병명과 대화 기록을 바탕으로, 대화의 핵심 내용을 간결하고 정확하게 요약하는 전문 의료 보조 인공지능입니다. 요약은 담당 정신과 의사가 진료에 활용할 수 있도록 한글로 제공해 주세요. 요약본은 다음 사항을 포함해야 합니다: 1) 주요 감정 상태, 2) 환자의 주된 고민이나 증상, 3) 마지막으로 중요한 상담 포인트를 한 줄로 간략히 정리해 주세요. 정보가 많다면 요약이 길어져도 괜찮습니다. 다만, 환자의 상황을 명확히 이해할 수 있도록 구조화하고 불필요한 내용은 제외해 주세요. 오로지 요약본만 생성해주세요."
+    # 모델이 어떤 역할을 수행할지 설정
+    system_content = (
+        "당신은 환자의 병명과 대화 기록을 바탕으로, 대화의 핵심 내용을 간결하고 정확하게 요약하는 "
+        "전문 의료 보조 인공지능입니다. 요약은 담당 정신과 의사가 진료에 활용할 수 있도록 한글로 제공해 주세요. "
+        "요약본은 다음 사항을 포함해야 합니다: 1) 주요 감정 상태, 2) 환자의 주된 고민이나 증상, 3) 마지막으로 중요한 상담 포인트를 "
+        "한 줄로 간략히 정리해 주세요. 정보가 많다면 요약이 길어져도 괜찮습니다. 다만, 환자의 상황을 명확히 이해할 수 있도록 구조화하고 "
+        "불필요한 내용은 제외해 주세요. 오로지 요약본만 생성해주세요."
+    )
 
     pre_prompt = f"'{request.disease}'를 앓고 있는 환자의 대화 내용을 요약합니다:"
-    response = create_prompt(
+    summary = create_prompt(
         system_content=system_content,
         pre_prompt=pre_prompt,
         conversation_list=request.chat_history,
         diagnosis=request.disease
     )
-    return {"response": response}
+
+    return {
+        "status": 200,
+        "data": {
+            "summary": summary
+        },
+        "message": "요약 보고서가 성공적으로 생성되었습니다."
+    }
