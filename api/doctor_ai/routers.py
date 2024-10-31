0abd6b8b1dff912227b321445e6992b8459a21bc
khyaejin <olivia019182@naver.com>
2024-11-01 03:33:55 +0900

    ✨ [Feat] openAI - 기본 세팅

--- api/doctor_ai/routers.py
+++ api/doctor_ai/routers.py
@@ -1,8 +1,12 @@
 # API 엔드포인트 정의, 서비스 레이어 호출해 요청 처리 (Spring Boot의 controller 같은 역할)
+from fastapi import APIRouter, HTTPException
+from starlette.responses import JSONResponse
 
-from fastapi import APIRouter
+from api.doctor_ai.models import AiChatRequest
+from openAI.gpt_service import create_prediction_prompt
 
 router = APIRouter()
+
 
 @router.get("/")
 async def root():
@@ -12,6 +16,21 @@ async def root():
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
-@router.post("/api/doctor_ai/chat")
-async def doctor_ai_chat():
-    return {"답변답변"}
+@router.post("/api/doctor-ai/chat")
+async def doctor_ai_chat(request: AiChatRequest):
+    try:
+        print(request.content)
+        content = create_prediction_prompt(request.content)
+        if content is None:
+            raise HTTPException(status_code=204, detail="Something went wrong")
+
+        response_data = {
+            "status": 200,
+            "data": content
+        }
+    except HTTPException as e:
+        response_data = {
+            "status": e.status_code,
+            "data": "죄송합니다. 오류로 인해 예상 질문이 생성되지 않았습니다. 다시 시도해주세요."
+        }
+    return JSONResponse(content=response_data)
--- openAI/gpt_service.py
+++ openAI/gpt_service.py
@@ -1,1 +1,37 @@
 # OpenAI 관련 로직
+
+import os
+import openai
+from dotenv import load_dotenv
+
+# 환경 변수 로드
+load_dotenv()
+
+# OpenAI 설정
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+# print("Loaded OPENAI_KEY:", OPENAI_KEY)  # 디버깅용-제대로 로드 됨
+
+MODEL = os.getenv("OPENAI_MODEL")
+# gpt 통신
+def post_gpt(system_content, user_content):
+    try:
+        openai.api_key = OPENAI_KEY
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": "시스템 메시지"},
+                {"role": "user", "content": "사용자 입력"}
+            ]
+        )
+        answer = response['choices'][0]['message']['content']
+        return answer
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+# 프롬프트 생성 - 예시
+def create_prediction_prompt(prompt):
+    system_content = "You are a helpful consulting assistant."
+    pre_prompt = "한국어로 답변해줘; 해당 자기소개서를 통해 나올 수 있는 예상 질문을 5개 출력해줘; 답변형식은 번호를 붙여서 답변만 한 문장씩 출력해줘; \n\n"
+    answer = post_gpt(system_content, pre_prompt + prompt)
+    return answer
