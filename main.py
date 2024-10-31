54ea66ec89cfdacdbb1184e6ae6b4e12b13c0819
khyaejin <olivia019182@naver.com>
2024-11-01 01:04:26 +0900

    ✨ [Feat] 기본 설정 완료 - 기본 구조 설정 완료

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# 예시 - 참고해서 코드 짜기 -----------------------
 @app.get("/")
 async def root():
     return {"message": "안녕하세요, FastAPI!"}
@@ -18,10 +19,22 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+# --------------------------------------------
+
+# 의사 AI 와의 채팅 기능
+@app.post("/api/doctor-ai/chat")
+async def doctor_ai_chat():
+    return {"답변답변"}
+
+
+
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
     try:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
+        uvicorn.run(app, host="0.0.0.0", port=8000) #8000 포트에서 실행
     except KeyboardInterrupt:
         print("프로그램이 사용자에 의해 중단되었습니다.")
